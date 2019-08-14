28945ee8dc367b3bf5218842eeae6c34dc613a71
miaurian <marian.mazurett@ug.uchile.cl>
2019-08-14 18:04:48 -0500

    a
    subiendo desafio 2

---
+++ miapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'miapp/index.html')
+
+def pestaña(request):
+	return render(request, 'miapp/pestaña.html')
+	
+def nombreseqdoc(request):
+	return render(request, 'miapp/nombreseqdoc.html')
